e2501b11c107bbfa65db16f232fb45b057aa15d6
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2023-07-22 12:36:13 +0200

    Added tests for mixed systems for struqture-py json_schema

--- struqture-py/tests/mixed_systems/mixed_decoherence_product.rs
+++ struqture-py/tests/mixed_systems/mixed_decoherence_product.rs
@@ -14,6 +14,8 @@ use num_complex::Complex64;
 use pyo3::prelude::*;
 use std::cmp::Ordering;
 use struqture::prelude::*;
+#[cfg(feature = "json_schema")]
+use struqture::STRUQTURE_VERSION;
 use struqture::{
     bosons::BosonProduct, fermions::FermionProduct, mixed_systems::MixedDecoherenceProduct,
     prelude::MixedIndex, spins::DecoherenceProduct,
@@ -599,5 +601,34 @@ fn test_hash() {
         let not_equal =
             bool::extract(hash_pp.call_method1("__eq__", (hash_other_pp,)).unwrap()).unwrap();
         assert!(!not_equal);
+    });
+}
+
+#[cfg(feature = "json_schema")]
+#[test]
+fn test_json_schema() {
+    pyo3::prepare_freethreaded_python();
+    pyo3::Python::with_gil(|py| {
+        let new = new_pp(
+            py,
+            vec!["0Z".to_string()],
+            vec!["c0a0".to_string()],
+            vec!["c1a1".to_string()],
+        );
+
+        let schema: String = String::extract(new.call_method0("json_schema").unwrap()).unwrap();
+        let rust_schema =
+            serde_json::to_string_pretty(&schemars::schema_for!(MixedDecoherenceProduct)).unwrap();
+        assert_eq!(schema, rust_schema);
+
+        let version: String =
+            String::extract(new.call_method0("current_version").unwrap()).unwrap();
+        let rust_version = STRUQTURE_VERSION.to_string();
+        assert_eq!(version, rust_version);
+
+        let min_version: String =
+            String::extract(new.call_method0("min_supported_version").unwrap()).unwrap();
+        let rust_min_version = String::from("1.0.0");
+        assert_eq!(min_version, rust_min_version);
     });
 }
