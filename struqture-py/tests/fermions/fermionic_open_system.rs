6094bfe22fd76b07d4529aad7354d2b0cbd1fa99
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2023-07-22 12:22:33 +0200

    Added struqture-py json_schema unittests for fermions

--- struqture-py/tests/fermions/fermionic_open_system.rs
+++ struqture-py/tests/fermions/fermionic_open_system.rs
@@ -24,6 +24,8 @@ use struqture_py::fermions::{
     FermionLindbladOpenSystemWrapper,
 };
 use test_case::test_case;
+#[cfg(feature = "json_schema")]
+use struqture::STRUQTURE_VERSION;
 
 // helper functions
 fn new_system(py: Python) -> &PyCell<FermionLindbladOpenSystemWrapper> {
@@ -1590,5 +1592,30 @@ fn test_jordan_wigner() {
         let number_spins =
             usize::extract(slos.call_method0("current_number_spins").unwrap()).unwrap();
         assert_eq!(number_modes, number_spins)
+    });
+}
+
+#[cfg(feature = "json_schema")]
+#[test]
+fn test_json_schema() {
+    pyo3::prepare_freethreaded_python();
+    pyo3::Python::with_gil(|py| {
+        let new = new_system(py);
+
+        let schema: String = String::extract(new.call_method0("json_schema").unwrap()).unwrap();
+        let rust_schema =
+            serde_json::to_string_pretty(&schemars::schema_for!(FermionLindbladOpenSystem)).unwrap();
+        assert_eq!(schema, rust_schema);
+
+        let version: String =
+            String::extract(new.call_method0("current_version").unwrap()).unwrap();
+        let rust_version = STRUQTURE_VERSION.to_string();
+        assert_eq!(version, rust_version);
+
+        new.call_method1("noise_add_operator_product", (("c0a0", "c0a0"), 1.0)).unwrap();
+        let min_version: String =
+            String::extract(new.call_method0("min_supported_version").unwrap()).unwrap();
+        let rust_min_version = String::from("1.0.0");
+        assert_eq!(min_version, rust_min_version);
     });
 }
