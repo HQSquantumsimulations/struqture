a3679002f0d48f7ea68889a9d088ceb979286be2
kbarkhqs <65908581+kbarkhqs@users.noreply.github.com>
2023-08-10 10:45:23 +0200

    Feature copyright (#95)
    * Fixed typo
    
    * Removed commented out code
    
    * First draft of pmo from shs
    
    * Updated version
    
    * Fixed import of MixedPlusMinus structs in struqture-py, updated version and fixed clippy in tests
    
    * Updated versions

--- struqture-py/struqture_py/__init__.py
+++ struqture-py/struqture_py/__init__.py
@@ -1,10 +1,10 @@
-# Copyright © 2019-2022 HQS Quantum Simulations GmbH. All Rights Reserved.
+# Copyright © 2019-2023 HQS Quantum Simulations GmbH. All Rights Reserved.
 
 """Struqture python interface
 
 `HQS Quantum Simulations <https://quantumsimulations.de>`_ package for representing physical operators.
 
-Copyright © 2021-2022 HQS Quantum Simulations GmbH. All Rights Reserved.
+Copyright © 2021-2023 HQS Quantum Simulations GmbH. All Rights Reserved.
 
 .. autosummary::
     :toctree: generated/
@@ -15,6 +15,6 @@ Copyright © 2021-2022 HQS Quantum Simulations GmbH. All Rights Reserved.
     mixed_systems
 
 """
-print("Struqture python interface\nCopyright © 2021-2022 HQS Quantum Simulations GmbH. All Rights Reserved.\n\n")
+print("Struqture python interface\nCopyright © 2021-2023 HQS Quantum Simulations GmbH. All Rights Reserved.\n\n")
 from .struqture_py import *
 __license__ = "Apache-2.0"
