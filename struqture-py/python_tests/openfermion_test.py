5bd3b1c2ccce9a8bf2e90b8d6100dd83534692af
Dimitri Berthault <dimitri.berthault@quantumsimulations.de>
2026-01-21 09:50:52 +0100

    Add more tests

--- struqture-py/python_tests/openfermion_test.py
+++ struqture-py/python_tests/openfermion_test.py
@@ -16,6 +16,14 @@ def test_openfermion_to_struqture():
     assert struqture_hamiltonian == openfermion_to_struqture(openfermion_hamiltonian)
 
 
+def test_openfermion_to_struqture_to_openfermion():
+    openfermion_hamiltonian = (
+        0.68 * QubitOperator("X0 Y1") + 0.1 * QubitOperator("Y0 Y1") - 0.5 * QubitOperator("X0 X1")
+    )
+    struqture_hamiltonian = openfermion_to_struqture(openfermion_hamiltonian)
+    assert openfermion_hamiltonian == struqture_to_openfermion(struqture_hamiltonian)
+
+
 def test_struqture_to_openfermion():
     struqture_hamiltonian = PauliHamiltonian()
     struqture_hamiltonian.add_operator_product("0X1Y", 0.68)
@@ -27,3 +35,14 @@ def test_struqture_to_openfermion():
     )
 
     assert openfermion_hamiltonian == struqture_to_openfermion(struqture_hamiltonian)
+
+
+def test_struqture_to_openfermion_to_struqture():
+    struqture_hamiltonian = PauliHamiltonian()
+    struqture_hamiltonian.add_operator_product("0X1Y", 0.68)
+    struqture_hamiltonian.add_operator_product("0X1X", -0.5)
+    struqture_hamiltonian.add_operator_product("0Y1Y", 0.1)
+
+    openfermion_hamiltonian = struqture_to_openfermion(struqture_hamiltonian)
+
+    assert struqture_hamiltonian == openfermion_to_struqture(openfermion_hamiltonian)
